032eeafe6e14b0fed4b98f5f73b90b4d9569a8ad
Katu <captain.katu@gmail.com>
2023-12-09 17:45:39 -0600

    12/9/23 changes
    mostly newly generated configs for new mods. but also a few datapacks, and I turned off some stuff in Iron's RPG tweaks. and added flowers to a forge tag.

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -4,4 +4,38 @@ ServerEvents.tags('item', event => {
         'forge:salt', 'meadow:alpine_salt'
     )
 
+    event.add(
+        'forge:flowers',
+        'minecraft:dandelion', 
+        'minecraft:poppy', 
+        'minecraft:blue_orchid', 
+        'minecraft:allium', 
+        'minecraft:azure_bluet', 
+        'minecraft:red_tulip', 
+        'minecraft:orange_tulip', 
+        'minecraft:white_tulip', 
+        'minecraft:pink_tulip', 
+        'minecraft:oxeye_daisy', 
+        'minecraft:cornflower', 
+        'minecraft:lily_of_the_valley', 
+        'minecraft:torchflower', 
+        'minecraft:wither_rose', 
+        'minecraft:spore_blossom', 
+        'minecraft:sunflower', 
+        'minecraft:lilac', 
+        'minecraft:rose_bush', 
+        'minecraft:peony', 
+        'regions_unexplored:cactus_flower', 
+        'aether:purple_flower', 
+        'aether:white_flower', 
+        'eidolon:oanna_bloom', 
+        'snifferent:spineflower', 
+        'regions_unexplored:orange_coneflower', 
+        'regions_unexplored:purple_coneflower', 
+        'regions_unexplored:hyacinth_flowers', 
+        'regions_unexplored:blue_magnolia_flowers', 
+        'regions_unexplored:pink_magnolia_flowers', 
+        'regions_unexplored:white_magnolia_flowers',
+    )
+
 })
